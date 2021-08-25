5607590aa1d980a6b37fce4e6815a4c950692f4c
Mauri <dronM27a.z@gmail.com>
2021-08-24 23:07:06 -0300

    Create constantes.js

---
+++ client/src/constantes.js
@@ -0,0 +1,6 @@
+export const BD_URL = 'http://localhost:3001/';
+export const URL_VIDEOGAMES = this.BD_URL + 'videogames';
+export const URL_GENRES = this.BD_URL + 'genres';
+export const URL_DETAIL_GAMES = this.BD_URL + 'videogame';
+
+
