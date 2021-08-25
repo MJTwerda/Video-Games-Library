e6587e4353babb3e112f053d581f6955541e0700
Mauri <dronM27a.z@gmail.com>
2021-08-24 22:49:59 -0300

    Update routeGenre.js

--- api/src/routes/routeGenre.js
+++ api/src/routes/routeGenre.js
@@ -1,1 +1,24 @@
+const express = require('express');
+const router = express.Router();
 
+router.use(express.json());
+
+const { Genre } = require('../db.js');
+
+const axios = require('axios');
+const { API_KEY } = process.env;
+
+router.get('/', async (req, res) => {
+    try {
+        let allGenres = await Genre.findAll({
+            attributes: ['id', 'name']
+        });
+
+        res.json(allGenres)
+
+    } catch(err) {
+        res.send(err)
+    }
+});
+
+module.exports = router;
