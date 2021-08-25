01f35f7badbb6e552420b80d0a2f9557b20d4ba5
Mauri <dronM27a.z@gmail.com>
2021-08-24 22:54:24 -0300

    Update index.js

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,1 +1,15 @@
+const { Router } = require('express');
+
+const router = Router();
+const gamesRouters = require('./routeGame.js');
+const genresRouters = require('./routeGenre.js');
+const gameDetailRouters = require('./routeDetailGame');
+const allPlatforms = require('./routePlatforms');
+
+router.use('/videogames', gamesRouters); 
+router.use('/genres', genresRouters);
+router.use('/videogame', gameDetailRouters);
+router.use('/platforms', allPlatforms);
+
+module.exports = router;
 
